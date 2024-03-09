28ef952d973d509e4feb6e1b9c8f2ea1ec741cd5
Davi Ferreira da Silva <sildv45@gmail.com>
2024-03-09 18:10:28 -0300

    Aplicavo com JavaScript

---
+++ index.js
@@ -0,0 +1,20 @@
+function on() {
+  document.getElementById("lamp").src = "on.jpg";
+}
+
+function off() {
+  document.getElementById("lamp").src = "off.jpg";
+}
+
+function blink() {
+  var intervalo = 0;
+  var contador = 0;
+  while (contador < 15) {
+    intervalo += 200;
+    setTimeout("document.getElementById('lamp').src='on.jpg'; ", intervalo);
+
+    intervalo += 300;
+    setTimeout("document.getElementById('lamp').src='off.jpg'; ", intervalo);
+    contador++;
+  }
+}
